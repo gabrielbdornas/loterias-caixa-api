4c48a4e15858ec074cc6c980f657d9e9a7a6aa87
AntonioMarcel <marcel.sotero@gmail.com>
2023-08-01 16:51:27 -0300

    cria datapackage, dataset e dataframe

--- test_pandas.py
+++ test_pandas.py
@@ -1,5 +1,14 @@
 from frictionless import Package
-package = Package('datapackage.yaml')
-mega = package.get_resource('mega-sena')
+import pandas as pd
+
+# df = pd.read_excel("jogos/Mega-Sena.xlsx", index_col=0)
+# df.to_csv("jogos/Mega-Sena.csv")
+
+
+package = Package("datapackage2.yaml")
+mega = package.get_resource("mega-sena")
 df = mega.to_pandas()
-import ipdb; ipdb.set_trace(context=10)
+
+import ipdb
+
+ipdb.set_trace(context=10)
