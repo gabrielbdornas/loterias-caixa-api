0cb83c0b406ccfd89701e3e2a80da0ab87aab058
AntonioMarcel <marcel.sotero@gmail.com>
2023-08-21 14:30:53 -0300

    alimenta base mega-sena

--- etl/list_games.py
+++ etl/list_games.py
@@ -6,12 +6,15 @@ urllib3.disable_warnings()
 
 BASE_URL = "https://servicebus2.caixa.gov.br/portaldeloterias/api/resultados/download"
 
+# network -> limpar -> clicar no botão de download
+# https://servicebus2.caixa.gov.br/portaldeloterias/api/resultados/download?modalidade=Mega-Sena
+
 jogos_list = [
     "+Milionária",
     "Mega-Sena",
     "Lotofácil",
     "Quina",
-#    "Lotomania",
+    #    "Lotomania",
     "Timemania",
     "Dupla-Sena",
     "Federal",
@@ -20,22 +23,21 @@ jogos_list = [
     "Super-Sete",
 ]
 
-
 for jogo in jogos_list:
     res = requests.get(BASE_URL, params={"modalidade": jogo}, verify=False)
 
     if res.status_code == 200:
-        with open(f'./data/excel/{jogo}.xlsx', 'wb') as file:
+        with open(f"../data/excel/{jogo}.xlsx", "wb") as file:
             file.write(res.content)
 
-            df = pd.read_excel(f'./data/excel/{jogo}.xlsx')
+            df = pd.read_excel(f"../data/excel/{jogo}.xlsx")
 
             # if jogo == "Lotomania":
             #     import ipdb; ipdb.set_trace(context=10)
 
-            df.to_csv(f'./data/csv/{jogo}.csv')
+            df.to_csv(f"../data/csv/{jogo}.csv")
 
-            print(f'{jogo}.xlsx baixado.')
+            print(f"{jogo}.xlsx baixado.")
         continue
 
-    raise Exception(f'Requisição falhou em {jogo}')
+    raise Exception(f"Requisição falhou em {jogo}")
--- etl/test_pandas.py
+++ etl/test_pandas.py
@@ -1,14 +1,13 @@
 from frictionless import Package
 import pandas as pd
 
-# df = pd.read_excel("jogos/Mega-Sena.xlsx", index_col=0)
-# df.to_csv("jogos/Mega-Sena.csv")
+df = pd.read_excel("../data/excel/Mega-Sena.xlsx", index_col=0)
+df.to_csv("../data/csv/Mega-Sena.csv")
+# frictionless describe ../data/csv/Mega-Sena.csv --yaml > datapackage.yaml #rodar no terminal para gerar o datapackage
+# package = Package("datapackage2.yaml")
+# mega = package.get_resource("mega-sena")
+# df = mega.to_pandas()
 
+# import ipdb
 
-package = Package("datapackage2.yaml")
-mega = package.get_resource("mega-sena")
-df = mega.to_pandas()
-
-import ipdb
-
-ipdb.set_trace(context=10)
+# ipdb.set_trace(context=10)
