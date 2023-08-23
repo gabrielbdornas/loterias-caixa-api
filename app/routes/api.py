ff010489a9981466312f7fbd25d4da129e1e820a
AntonioMarcel <marcel.sotero@gmail.com>
2023-08-22 21:40:46 -0300

    cria rota mega-sena

--- app/routes/api.py
+++ app/routes/api.py
@@ -16,17 +16,25 @@ def health_check():
 
 @api_bp.get("/mega-senas")
 def get_mega_sena():
-    mega_senas = MegaSena.find_all()
+    mega_senas = MegaSena().find_all()
     schema = MegaSenaSchema(many=True)
     return jsonify(schema.dump(mega_senas))
 
 
 @api_bp.get("/mega-senas/<int:id>")
-def get_mega_sena_teste(id: int):
+def get_mega_sena_by_id(id: int):
     mega_sena = MegaSena.find_by_id(id)
 
     if mega_sena is None:
-        return {"message": "Jogo não encontrado"}, 404
+        return {"message": "Não existe jogo com esse id"}, 404
 
     schema = MegaSenaSchema()
     return jsonify(schema.dump(mega_sena))
+
+
+# @api_bp.get("/mega-senas/<id:int>")
+# def get_mega_sena_by_id():
+#     mega_sena = MegaSena()
+#     mega_senas = mega_sena.query.all()
+#     schemas = MegaSenaSchema(many=True)
+#     return jsonify(schemas.dump(mega_senas)), 200
